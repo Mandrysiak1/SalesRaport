f73a97af97d960644443f44c543be7334f78977e
mandrysiak <andrysiakmaciejj@gmail.com>
2022-08-04 10:04:54 +0200

    change

--- server.js
+++ server.js
@@ -11,13 +11,5 @@ app.get('/', (req,res) =>{
 const ordersRouter = require("./routes/orders")
 
 app.use("/orders", ordersRouter)
-const Port = 8080
+const Port = process.env.PORT  || 8080
 app.listen(Port,() => console.log(`serwer start ${Port}`))
-
-// var http = require('http');
-// http.createServer(function (req, res) {
-// console.log("serwer start")
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-// res.end('Hello World !');
-
-// }).listen(8080);
