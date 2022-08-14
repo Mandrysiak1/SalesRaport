e42eb7e2a769175886be2ebf6f5f241eee81d05e
mandrysiak <andrysiakmaciejj@gmail.com>
2022-08-14 20:59:34 +0200

    wip

--- routes/shipments.js
+++ routes/shipments.js
@@ -7,132 +7,195 @@ router.get('/get', async (req, res) => {
 
   let order_id = req.body.order_id
 
- 
-    res.send("test");
+
+  res.send("test");
 })
 
 
-router.get('/execute', async (req,res) =>{
+router.get('/execute', async (req, res) => {
 
-  let orderID = 49514944
+  let orderID = 51983622
 
   //mark package with "api" in extrafield1
+  //create all nessesery packages
+  //unmark package
+  //move order to proper category
+  //send emails
 
-//create all nessesery packages
-//unmark package
-//move order to proper category
-//send emails
- 
-  await getOrderDetails(orderID)
+  let orderDetails =  await getOrderDetails(orderID)
 
-  // await markOrderWithStar(orderID)
-  // await createPackages(orderID)
-  // await unmarkOrderWithStar(orderID)
+ // console.log(orderDetails.orders[0].products)
 
-//  await unmarkOrderWithStar(orderID)
-//  await moveOrderToProperCategory(orderID)
-//  await sendEmail(orderID)
 
-res.send("ok")
+
+
+  //await markOrderWithStar(orderID)
+  await createPackages(orderID,orderDetails)
+  //await unmarkOrderWithStar(orderID)
+  //await moveOrderToProperCategory(orderID,orderDetails.order_source)
+  //await sendEmail(orderID)
+
+  res.send("ok")
 
 })
 
-async function createPackages(orderID)
-{
+async function createPackages(orderID,orderDetails) {
+
+//check what type of courier
+  //if allegro check "courier" -create array
+  //if paczkomaty get size from Form
+  //if kurier inpost check
+  //if dhl check
+//check if cod, add to "fields", calculate cod value
+//calculate insurance
+//if not paczkomaty prepare "package_fields"
+
+  
 
   let fields = []
   let package_fields = []
 
-  fields.push(
-  {id:"courier", value:"9685250"},
-  {id:"package_type",value:"PACKAGE"},
-  {id:"insurance",type: 42.85 },
-  {id:"package_description", value:orderID},
-  {id:"reference_number",value:orderID}
-  )
-  package_fields.push(
-    {weight:2},
-    {size_length:30},
-    {size_width:40},
-    {size_height:50}
-  )
+  checkIfCod(fields,orderDetails)
+  getInsuranceValue(fields,orderDetails)
 
-  let params = {
-    "order_id":orderID ,
-    "courier_code":"allegrokurier",
-    "fields" : fields,
-    "packages": package_fields
-  };
+  console.log(fields)
 
-  let data = {
-    'method': 'createPackage',
-    'parameters': JSON.stringify(params)
-  };
-  var initdata = await axios
-    .post('https://api.baselinker.com/connector.php', data, { headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
+  // fields.push(
+  //   { id: "courier", value: "9685250" },
+  //   { id: "package_type", value: "PACKAGE" },
+  //   { id: "insurance", type: 42.85 },
+  //   { id: "package_description", value: orderID },
+  //   { id: "reference_number", value: orderID },
+  //   //{ id: "cod",value:"tbd"}
+  // )
+  // package_fields.push(
+  //   { weight: 2 },
+  //   { size_length: 30 },
+  //   { size_width: 40 },
+  //   { size_height: 50 }
+  // )
 
-    console.log(initdata)
+  // let params = {
+  //   "order_id": orderID,
+  //   "courier_code": "allegrokurier",
+  //   "fields": fields,
+  //   "packages": package_fields
+  // };
 
+  // let data = {
+  //   'method': 'createPackage',
+  //   'parameters': JSON.stringify(params)
+  // };
+  // var initdata = await axios
+  //   .post('https://api.baselinker.com/connector.php', data, { headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
 
-
-    // KONFIG INPOST
-    // fields.push({id:"service",value:"detect_new"},{id: "size_type",value:"B", }, {id:"insurance",type: 40 })
-
-    // let params = {
-    //   "order_id":orderID ,
-    //   "courier_code":"paczkomaty",
-    //   "fields" : fields,
-    //   "packages": packages
-    // };
-  
-    // let data = {
-    //   'method': 'createPackage',
-    //   'parameters': JSON.stringify(params)
-    // };
-    // var initdata = await axios
-    //   .post('https://api.baselinker.com/connector.php', data, { headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
-  
-    //   console.log(initdata)
+  // console.log(initdata)
 
 
-}
 
-async function markOrderWithStar(orderID){
+  // // KONFIG INPOST paczkomaty
+  //  fields.push(
+  //   {id:"service",value:"detect_new"},
+  //   {id: "size_type",value:"B", }, 
+  //   {id:"insurance",type: 40 },
+  //   //{ id: "cod",value:"tbd"}
+  //   )
 
-  let params = {
-    "order_id": orderID,
-    "extra_field_1":"api"
-  };
+  // let params = {
+  //   "order_id":orderID ,
+  //   "courier_code":"paczkomaty",
+  //   "fields" : fields,
+  //   "packages": packages
+  // };
 
-  let data = {
-    'method': 'setOrderFields',
-    'parameters': JSON.stringify(params)
-  };
-   await axios
-   .post('https://api.baselinker.com/connector.php', data, {
-     headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
+  // let data = {
+  //   'method': 'createPackage',
+  //   'parameters': JSON.stringify(params)
+  // };
+  // var initdata = await axios
+  //   .post('https://api.baselinker.com/connector.php', data, { headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
+
+  //   console.log(initdata)
+
 
 }
 
-async function unmarkOrderWithStar(orderID){
-
-  let params = {
-    "order_id": orderID,
-    "extra_field_1":""
-  };
-
-  let data = {
-    'method': 'setOrderFields',
-    'parameters': JSON.stringify(params)
-  };
-   await axios
-   .post('https://api.baselinker.com/connector.php', data, {
-     headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
-
-}
-
-async function getOrderDetails(orderID)
+function checkIfCod(fields,orderDetails)
 {
+  console.log(orderDetails.orders[0])
+  if(orderDetails.orders[0].payment_method_cod === '0') /// ????
+  {
+    return fields;
+  }else{
+
+    let codValue = 0
+
+    for (let i = 0; i < orderDetails.orders[0].products.length; i++){
+      var quantity = orderDetails.orders[0].products[i].quantity
+      var price = orderDetails.orders[0].products[i].price_brutto
+
+      codValue += (quantity * price)
+     }
+  
+     codValue += orderDetails.orders[0].delivery_price
+     return fields.push({id: "cod", value : codValue.toFixed(2)})
+    
+  }
+
+}
+
+function getInsuranceValue(fields,orderDetails)
+{
+  let insuranceValue = 0
+
+    for (let i = 0; i < orderDetails.orders[0].products.length; i++){
+      var quantity = orderDetails.orders[0].products[i].quantity
+      var price = orderDetails.orders[0].products[i].price_brutto
+
+      insuranceValue += (quantity * price)
+     }
+
+     return fields.push({id: "insurance", value : insuranceValue.toFixed(2)})
+
+}
+
+async function markOrderWithStar(orderID) {
+
+  let params = {
+    "order_id": orderID,
+    "extra_field_1": "api"
+  };
+
+  let data = {
+    'method': 'setOrderFields',
+    'parameters': JSON.stringify(params)
+  };
+  await axios
+    .post('https://api.baselinker.com/connector.php', data, {
+      headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' }
+    })
+
+}
+
+async function unmarkOrderWithStar(orderID) {
+
+  let params = {
+    "order_id": orderID,
+    "extra_field_1": ""
+  };
+
+  let data = {
+    'method': 'setOrderFields',
+    'parameters': JSON.stringify(params)
+  };
+  await axios
+    .post('https://api.baselinker.com/connector.php', data, {
+      headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' }
+    })
+
+}
+
+async function getOrderDetails(orderID) {
   let params = {
     "order_id": orderID,
   };
@@ -141,20 +204,49 @@ async function getOrderDetails(orderID)
     'method': 'getOrders',
     'parameters': JSON.stringify(params)
   };
- let info =   await axios
-   .post('https://api.baselinker.com/connector.php', data, {
-     headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' } })
+  let info = await axios
+    .post('https://api.baselinker.com/connector.php', data, {
+      headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' }
+    })
 
 
-     console.log(info.data.orders[0].products)
+  return info.data
+
+}
+
+async function sendEmail() {
+
+
 
 }
 
-async function sendEmail()
-{
+async function moveOrderToProperCategory(orderID,orderSource) {
+
+  let statusID = getCategoryStatus(orderSource)
+
+  let params = {
+    "order_id": orderID,
+    "status_id": statusID
+  };
+
+  let data = {
+    'method': 'setOrderStatus',
+    'parameters': JSON.stringify(params)
+  };
+  let info = await axios
+    .post('https://api.baselinker.com/connector.php', data, {
+      headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' }
+    })
 
 
-
+  console.log(info.status)
 }
+
+async function getCategoryStatus(orderSource) {
+  return sourceToCategoryArray[orderSource];
+}
+
+
+const sourceToCategoryArray = { "allegro": 161878, "shopee": 173349, "empik": 193321, "morele": 194350, "cenoe": 239827 }
 
 module.exports = router
