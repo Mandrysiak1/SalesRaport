c130ded91f1e48c805d5da01ba6b83e58b230a12
mandrysiak <andrysiakmaciejj@gmail.com>
2022-08-29 12:34:21 +0200

    erer'

--- routes/shipments.js
+++ routes/shipments.js
@@ -2,18 +2,29 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 const { ConsoleMessage } = require('puppeteer')
+var nodemailer = require('nodemailer');
 router.use(require('body-parser').json());
-require('js-base64').Base64.atob()
+const fs = require('fs').promises;
+
 const { getOrderDetails, checkIfCod, getInsuranceValue } = require('./functions');
+const { rootCertificates } = require('tls');
 
-router.post('/email', async (req,res) => {
+router.get('/email', async (req,res) => {
 
-  let emailTopic = ''
-  let emailContent = ''
+  let emailTopic = 'eo'
+  let emailContent = 'xd'
   let emailAdresses = []
   let labelNumbers = [] //to co do remove
-  await getLabel(labelNumbers)
-  await sendEmail(emailTopic,emailContent,emailAdresses)
+
+  labelNumbers.push({courierCode: "paczkomaty", package_number:'642244367266620124418898',package_id :'36190738'})
+
+  let labels = await getLabels(labelNumbers)
+  console.log(labels)
+  
+  await sendEmail(emailTopic,emailContent,emailAdresses,labels)
+
+  res.json( {status:"SUCCESS"})
+
 })
 
 router.post('/create', async (req, res) => {
@@ -55,7 +66,7 @@ router.post('/remove', async (req, res) => {
   let package_number = req.body.package_number
   let courier_code = req.body.courier_code
 
-  let response = await removePackage(courier_code, package_id, package_number)
+ // let response = await removePackage(courier_code, package_id, package_number)
 
   console.log(courier_code, package_number, package_id)
 
@@ -360,21 +371,77 @@ async function unmarkOrderWithStar(orderID) {
 
 }
 
-async function sendEmail() {
+async function sendEmail(emailTopic,emailContent,emailAdresses,labels) {
 
+  let attachments = []
+  
 
+  labels.forEach(element => {
+    let x = new Object();
+    x.filename = element
+    x.path = './' + element
+    attachments.push(x)
+  })
+
+  console.log(attachments)
+
+  var mail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'family24raports@gmail.com',
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  emailAdresses.push('andrysiakmaciejj@gmail.com')
+  var maillist = emailAdresses
+
+  var mailOptions = {
+    from: 'givemesomething9@gmail.com',
+    to: maillist,
+    subject:emailTopic,
+    text: emailContent,
+    attachments: attachments
+  };
+
+  mail.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  return "success"
 
 }
+async function getLabels(labelNumbers){
 
-async function getLabel(labelNumbers){
+  let resposes = []
 
-  console.log("orderSource from moveorder: " + orderSource)
-  let statusID = getCategoryStatus(orderSource)
-  console.log("statusID from moveorder: " + statusID)
+  for(let element of labelNumbers){
+    let courierCode = element.courierCode
+    let package_id = element.package_id
+    let package_number = element.package_number
+
+    let res = await getLabel(courierCode,package_id,package_number)
+    resposes.push(res)
+  }
+
+  console.log(resposes)
+
+  if(resposes.some(e => e === 'fail')){
+    return 'fail'
+  }else {
+    return resposes
+  }
+
+}
+async function getLabel(courierCode,package_id,package_number){
 
   let params = {
-    "courier_code": orderID,
-    "package_id": statusID,
+    "courier_code": courierCode,
+    "package_id": package_id,
     "package_number" : package_number
   };
 
@@ -382,15 +449,36 @@ async function getLabel(labelNumbers){
     'method': 'getLabel',
     'parameters': JSON.stringify(params)
   };
-  let info = await axios
+  let res = await axios
     .post('https://api.baselinker.com/connector.php', data, {
       headers: { "X-BLToken": process.env.BASELINKER_API_KEY, 'Content-Type': 'multipart/form-data' }
     })
 
-
-  console.log("status changes: " + info.status)
-
+    console.log(res)
+    
+    return await saveBase64toPdf(res.data.label,package_id)
+  
 }
+
+async function saveBase64toPdf(base64code,filename){
+
+  let finalFilename = "Label" + filename + ".pdf"
+  console.log("err",finalFilename)
+
+  await fs.writeFile(finalFilename, base64code, 'base64', error =>  {
+
+    if (error) {
+        return "fail"
+
+    } else {
+        return finalFilename
+    }
+  });
+
+  return finalFilename
+}
+
+
 async function moveOrderToProperCategory(orderID, orderSource) {
 
   console.log("orderSource from moveorder: " + orderSource)
